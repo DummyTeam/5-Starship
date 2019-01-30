608587b0a24805cc8c541d82884572739e870918
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-01-30 14:07:21 +0400

    Initial commit

---
+++ 5. Starship2D/5. Starship2D/Main.cpp
@@ -0,0 +1,25 @@
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
+
+int main() {
+	RenderWindow window(VideoMode(200, 200), "Starship 2D");
+	CircleShape shape(100.f);
+	shape.setFillColor(Color::Green);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
